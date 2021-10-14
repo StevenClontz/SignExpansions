3634c334900c4dd70b94b7bbfd758dff0e1ecde1
Dylan Johnson <44678812+Dylan-Johnson@users.noreply.github.com>
2021-10-14 13:56:19 -0500

    Create FindSignExpansion.py

---
+++ FindSignExpansion.py
@@ -0,0 +1,58 @@
+import math
+
+def calcBinary(s):
+  return (1 / (2 ** (s)))
+
+def invert(se):
+  new_se = ""
+  for element in se:
+    if element == "R":
+      new_se += "L"
+    elif element == "L":
+      new_se += "R"
+  return new_se
+
+def se(num, steps = None, stop = False):
+  se = ""
+  placeholdernum = num
+  num_se = (math.modf(float(abs(placeholdernum))))
+
+
+  # Integer
+  se = se.ljust(int(num_se[1]) + len(se), "R")
+
+  # Decimal
+  if num_se[0] == 0.0:
+    if num < 0: return invert(se)
+    return (se)
+  se += "R"
+
+  bse = "L"
+  binary = num_se[0]
+  s = 1
+  treeVal = 0
+  if steps is None:
+    pass
+  elif steps > 0: 
+    while s <= steps and binary != 0: 
+      treeVal += calcBinary(s) 
+      #print(binary,treeVal)
+      if binary == treeVal:
+        #print("a")
+        break
+      elif binary > treeVal:
+        #print("b")
+        bse += "R"
+      elif binary < treeVal:
+        #print("c")
+        bse += "L"
+        treeVal -= 2 * calcBinary(s)/2
+      s += 1
+    if stop is True and binary != treeVal:
+      return "Error"
+
+  if num < 0: return invert(se+bse)
+  return(se + bse)
+
+
+print(se(-3.125,3, True))
